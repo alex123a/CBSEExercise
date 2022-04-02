28336972eaf8e3547233e84283910f425fee23e4
Alex Valentiner <alex.valentiner@hotmail.com>
2022-04-02 18:18:40 +0200

    Copied my enemy from netbeanslab1 to netbeanslab2

--- AsteroidsNetbeansModules/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyControlSystem.java
+++ AsteroidsNetbeansModules/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyControlSystem.java
@@ -1,76 +1,91 @@
 package dk.sdu.mmmi.cbse.enemysystem;
 
+import dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
-import dk.sdu.mmmi.cbse.common.data.entityparts.LifePart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
 import dk.sdu.mmmi.cbse.common.enemy.Enemy;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import java.util.Random;
+
+import org.openide.util.Lookup;
 import org.openide.util.lookup.ServiceProvider;
 
 @ServiceProvider(service = IEntityProcessingService.class)
 public class EnemyControlSystem implements IEntityProcessingService {
 
-    private Entity enemy;
+    Random random = new Random();
+    int whatToDo = random.nextInt(3);
+    int turns = 0;
+    int turnsBeforeChange = 60;
+    int fireRan = random.nextInt(50);
 
     @Override
     public void process(GameData gameData, World world) {
-
-        for (Entity enemy : world.getEntities(Enemy.class)) {
+        for (Entity enemy: world.getEntities(Enemy.class)) {
             PositionPart positionPart = enemy.getPart(PositionPart.class);
             MovingPart movingPart = enemy.getPart(MovingPart.class);
-            LifePart lifePart = enemy.getPart(LifePart.class);
-
-            Random rand = new Random();
-
-            float rng = rand.nextFloat();
-
-            if (rng > 0.1f && rng < 0.9f) {
-                movingPart.setUp(true);
-            }
-
-            if (rng < 0.2f) {
+            if (whatToDo == 0) {
                 movingPart.setLeft(true);
+                movingPart.setRight(false);
+                movingPart.setUp(false);
+                turns++;
+                if (turns % turnsBeforeChange == 0) {
+                    whatToDo = random.nextInt(4);
+                }
+            } else if (whatToDo == 1) {
+                movingPart.setRight(true);
+                movingPart.setLeft(false);
+                movingPart.setUp(false);
+                turns++;
+                if (turns % turnsBeforeChange == 0) {
+                    whatToDo = random.nextInt(4);
+                }
+            } else {
+                turns++;
+                movingPart.setUp(true);
+                movingPart.setLeft(false);
+                movingPart.setRight(false);
+                if (turns % (turnsBeforeChange) == 0) {
+                    whatToDo = random.nextInt(4);
+                }
             }
 
-            if (rng > 0.8f) {
-                movingPart.setRight(true);
+            if (fireRan == 0) {
+                Entity bullet = Lookup.getDefault().lookup(BulletSPI.class).createBullet(enemy, gameData);
+                world.addEntity(bullet);
             }
+
+            fireRan = random.nextInt(50);
 
             movingPart.process(gameData, enemy);
             positionPart.process(gameData, enemy);
-            lifePart.process(gameData, enemy);
 
             updateShape(enemy);
-
-            movingPart.setRight(false);
-            movingPart.setLeft(false);
-            movingPart.setUp(false);
         }
     }
 
     private void updateShape(Entity entity) {
-        float[] shapex = new float[4];
-        float[] shapey = new float[4];
+        float[] shapex = entity.getShapeX();
+        float[] shapey = entity.getShapeY();
         PositionPart positionPart = entity.getPart(PositionPart.class);
         float x = positionPart.getX();
         float y = positionPart.getY();
         float radians = positionPart.getRadians();
 
-        shapex[0] = (float) (x + Math.cos(radians) * entity.getRadius());
-        shapey[0] = (float) (y + Math.sin(radians) * entity.getRadius());
+        shapex[0] = (float) (x + Math.cos(radians) * 8);
+        shapey[0] = (float) (y + Math.sin(radians) * 8);
 
-        shapex[1] = (float) (x + Math.cos(radians - 4 * 3.1415f / 5) * entity.getRadius());
-        shapey[1] = (float) (y + Math.sin(radians - 4 * 3.1145f / 5) * entity.getRadius());
+        shapex[1] = (float) (x + Math.cos(radians - 4 * 3.1415f / 5) * 8);
+        shapey[1] = (float) (y + Math.sin(radians - 4 * 3.1145f / 5) * 8);
 
-        shapex[2] = (float) (x + Math.cos(radians + 3.1415f) * entity.getRadius() * 0.5);
-        shapey[2] = (float) (y + Math.sin(radians + 3.1415f) * entity.getRadius() * 0.5);
+        shapex[2] = (float) (x + Math.cos(radians + 3.1415f) * 5);
+        shapey[2] = (float) (y + Math.sin(radians + 3.1415f) * 5);
 
-        shapex[3] = (float) (x + Math.cos(radians + 4 * 3.1415f / 5) * entity.getRadius());
-        shapey[3] = (float) (y + Math.sin(radians + 4 * 3.1415f / 5) * entity.getRadius());
+        shapex[3] = (float) (x + Math.cos(radians + 4 * 3.1415f / 5) * 8);
+        shapey[3] = (float) (y + Math.sin(radians + 4 * 3.1415f / 5) * 8);
 
         entity.setShapeX(shapex);
         entity.setShapeY(shapey);
